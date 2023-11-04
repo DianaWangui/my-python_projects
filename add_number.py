b3ffb5ec8ee419791614ad8eea34a32c41038333
DianaWangui <dianawanguikt048@gmail.com>
2023-11-05 00:48:46 +0300

    Adding two numbers enters by users as strings

---
+++ add_number.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#  Enter the two numbers but as strings
+num = input("Enter a number: ")
+#  converting each number to an integer for adding
+num1 = int(num[0])
+num2 =int(num[1])
+#  printing the sum of the two numbers
+print(num1 + num2)
+

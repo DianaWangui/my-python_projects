157745db08c2d48a82f6bd0be2a1e4edc39ff6a5
DianaWangui <dianawanguikt048@gmail.com>
2023-11-05 01:07:21 +0300

    Calulating bmi as a whole number(int)

---
+++ bmi.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+height = input("Enter the height; ")
+weight = input("Enter the weight: ")
+height = float(height)
+weight = int(weight)
+bmi = weight / (height * height)
+bmi = int(bmi)
+print(bmi)

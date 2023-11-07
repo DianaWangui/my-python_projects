5389ab2fa4ba3fd69d55bb1914a268af10c7d86b
DianaWangui <dianawanguikt048@gmail.com>
2023-11-07 17:46:22 +0300

    Dealing with more edge cases like when user doesnt add more extra

--- order_pizza_project.py
+++ order_pizza_project.py
@@ -4,17 +4,16 @@ def welcome_menu():
     small_pizza = 800
     medium_pizza = 1200
     large_pizza = 1800
-    #  EXTRAS
-    pepperoni = 30
-    cheese = 50
 
     print(f"Choose 1 for Small pizza = {small_pizza}\n")
     print(f"Choose 2 for Medium Pizza = {medium_pizza}\n")
     print(f"Choose 3 for Large pizza = {large_pizza}\n")
     choice = int(input("Enter Your Choice Here: "))
-    my_Choice(choice)
 
-def my_Choice(choice):
+    my_Choice(choice, small_pizza, medium_pizza, large_pizza)
+
+
+def my_Choice(choice, small_pizza, medium_pizza, large_pizza):
     if choice == 1:
         print("You ordered Small Pizza ")
     elif choice == 2:
@@ -24,25 +23,35 @@ def my_Choice(choice):
     else:
         print("You entered an invalid choice!! Try again\n!")
         welcome_menu()
-    extras(choice)    
-def extras(choice):
+
+    extras(choice, small_pizza, large_pizza, medium_pizza)
+
+
+def extras(choice, small_pizza, large_pizza, medium_pizza):
     pepperoni = 50
     cheese = 20
 
-    total_small_pizza = 800 + 50
-    total_medium_pizza = 1200 + 50
-    total_large_pizza = 1800 + 50
+    totalForSmall= small_pizza + pepperoni + cheese
+    totalForMedium = medium_pizza + pepperoni + cheese
+    totalForLarge = large_pizza + pepperoni + cheese
 
     add_extra = input("Do you want to add extra Cheese and Pepperoni? Y/N ")
-    if add_extra == 'Y':
+    if add_extra == 'Y' or 'y':
         if choice == 1:
-            print("Your new bill is now 850\n")
+            print(f"Your new bill is now {totalForSmall}\n")
         elif choice == 2:
-            print("Your new bill is now 1250\n")
+            print(f"Your new bill is now {totalForMedium}\n")
         else:
-            print("Your new bill is 1850\n")
+            print(f"Your new bill is {totalForLarge}\n")
+        print("Lets now implement payment and checkout")
     else:
         print("You did not add any extras")
+        add_extra_again = input("Do you still want to add extras? Y/N")
+        if add_extra_again == 'y' or 'Y':
+            extras(choice, small_pizza, large_pizza, medium_pizza)
+        else:
+            print("Now lets work on Payments")
+            # Add function for payment and checkout
 
-        # Add function for checkout
+
 welcome_menu()

b17f38738778c1c0c73cd41cbf0743312d5270f8
DianaWangui <dianawanguikt048@gmail.com>
2023-11-11 12:51:49 +0300

    Correcting function call under invalid user choice function and adding comments

--- rock_paper_scissors_project.py
+++ rock_paper_scissors_project.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
 
+# This is an imported module for a random numberchoice
+# that will be used to check the computer moves
+
 import random
 
+# Function for the computer move using the random imported module
 def computers_choice():
     computer_choice = random.randrange(0, 3)
     print("Choices:\n0 for Rock\n1 for Paper\n2 for Scissor\n")
     print(f"Computers choice is: {computer_choice}.")
-    invalid_user_choice(computer_choice)
-    #return computer_choice
+    return invalid_user_choice(computer_choice)
 
+
+# Function for users invalid input (Not including characters) 
 def invalid_user_choice(computer_choice):
     #print("Choices:\n1 for Rock\n2 for Paper\n3 for Scissor")
     user_choice = int(input("Users choice 0, 1, 2: "))
     if user_choice > 2 or user_choice < 0:
-        print("Invalid input")
-        return computers_choice()
+        print("\nInvalid input. Enter Choice again!!\n")
+        return invalid_user_choice(computer_choice)
     else:
         return  valid_user_choice(user_choice, computer_choice)
 
+
+# Function for users valid input, this will now play the game
+# This is where computer choice and user choice are compared
 def valid_user_choice(user_choice, computer_choice):
 
     if user_choice == 1 and computer_choice == 0:
@@ -35,4 +43,6 @@ def valid_user_choice(user_choice, computer_choice):
     else:
         print("You draw")
 
+# To be updated
+# I will implement a function where the user input is not an integer
 computers_choice()

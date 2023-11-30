f524b9bc597a4a505077b825b91261bfaf7dc2a9
DianaWangui <dianawanguikt048@gmail.com>
2023-11-30 09:13:51 +0300

    adding area of a rectangle

--- areas.py
+++ areas.py
@@ -1,3 +1,6 @@
+"""This is a class to find area and perimeter of a circle 
+using class and attribute and method instantiations"""
+
 class Circle:
   pi = 3.14
   def __init__(self, radius=0):
@@ -15,11 +18,62 @@ class Circle:
     self.__radius = value
   
   def area_circle(self):
-    return 2 * Circle.pi * self.__radius **2
+    return Circle.pi * self.__radius **2
   def circumference_circle(self):
     return 2 * Circle.pi * self.__radius
+  # Testing the code
+if __name__ == "__main__":
+  c = Circle()
+  print(f'Area of the circle with no radius given is {c.area_circle()}')
+  print(f'Circumference of the circle with no radius given is {c.circumference_circle()}')
+  c.radius = 5
+  print(f'Area of the circle with radius 5 is {c.area_circle()}')
+  print(f'Circumference of the circle with radius 5 is {c.circumference_circle()}')
+
+
+"""A class rectangle to find area and perimeter
+of a triangke using properties like getter 
+to get the lenght and height and setter
+the values"""
+
+# This print is to print an empty line to separate outputs
+print()
+
+class Rectangle:
+  def __init__(self, length=0, width=0):
+    self.__length = length
+    self.__width = width
+  @property
+  def length(self):
+    return self.__length
+  @length.setter
+  def length(self, value):
+    if not isinstance(value, int):
+      raise TypeError("Length should be an integer")
+    if value <= 0:
+      raise ValueError("Length should be greater than zero")
+    self.__length = value
+  @property
+  def width(self):
+    return self.__width
+  @width.setter
+  def width(self, value):
+    if not isinstance(value, int):
+      raise TypeError("Width should be an integer")
+    if value <= 0:
+      raise ValueError("Width should be greater than zero")
+    self.__width = value
+  def area_rectangle(self):
+    return self.__length * self.__width
+  def perimeter_rectangle(self):
+    return (self.__length + self.__width) * 2
   
-circle_1 = Circle(4)
-print(f"Area is: {circle_1.area_circle()}")
-#circle_1 = Circle(4)
-print(circle_1.circumference_circle())
+  # Testing the code
+if __name__ == "__main__":
+  r = Rectangle()
+  print(f'Area of the rectangle with no dimensions given is {r.area_rectangle()}')
+  print(f'Perimeter of the rectangle with no dimensions given is {r.perimeter_rectangle()}')
+  r.length = 4
+  r.width = 6
+  print(f'Area of the rectangle with length 4 and width 6 is {r.area_rectangle()}')
+  print(f'Perimeter of the rectangle with length 4 and width 6 is {r.perimeter_rectangle()}')

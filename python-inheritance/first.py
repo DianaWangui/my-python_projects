cd75de2d43e7ac7927a573fa1d4a7870235ac1a8
DianaWangui <dianawanguikt048@gmail.com>
2023-12-05 10:46:30 +0300

    first inheritance task of a boy class inhertance

---
+++ python-inheritance/first.py
@@ -0,0 +1,27 @@
+class Human:
+  def eat(self):
+    print("I can eat")
+  def work(self):
+    print("I can work")
+
+class Male():
+  def flirt(self):
+    print("I can flirt")
+  def work(self):
+    print("I can code")
+
+class Boy(Human, Male):
+  pass
+
+# First object of our inheritance class Boy
+boy_1 = Boy()
+
+# calling the first method here
+boy_1.flirt()
+
+# obtainig method work() from the class Male without
+# printing one in Human class by default
+Male.work(boy_1)
+
+# How to see how method are arranged in order
+print(Boy.mro())
